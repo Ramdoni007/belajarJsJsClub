c1507b9e542e1c71a48a915f324b9962eac86915
Ramdoni007 <ramd42156@gmail.com>
2020-10-30 13:51:44 +0700

    Penambahan Materi Switch Case

--- 12 Switch Statment/app.js
+++ 12 Switch Statment/app.js
@@ -1,9 +1,53 @@
-console.log("Hellow DOWNY");
-console.log(12345);
-console.log(true);
-var greeting = "Salam Kenal!";
-console.log(greeting);
-console.log([1, 2, 3, 4]); // Ini Arrays
-console.log({ a: 1, b: 2, c: 3 });
-console.table({ a: 1, b: 2, c: 3 });
-console.error("This Is Error");
+// Switch Statment biasa di gunakan untuk melakukan proses If sederhana atau Tidak sama dengan
+
+// Contoh nya bisa kita liat sama sama di bawah ini :)
+
+const myLove = "Rem";
+
+switch (myLove) {
+  case "Maimunah":
+    console.log(`${myLove} adalah myLove Pangeran Downy-Kun`);
+    break;
+
+  case "Milea":
+    console.log(`${myLove} adalah myLove Pangeran Downy-Kun`);
+    break;
+  case "Rem":
+    console.log(`${myLove} adalah myLove Pangeran Downy-Kun`);
+    break;
+  default:
+    console.log("tidak ada di kedua nya ini");
+    break;
+}
+
+// Melakukan Pengecekan Jika Pada Nama Hari :)
+let day;
+
+switch (new Date().getDay()) {
+  case 0:
+    day = "Minggu";
+    break;
+  case 1:
+    day = "Senen";
+    break;
+  case 2:
+    day = "selasa";
+    break;
+  case 3:
+    day = "rebo";
+    break;
+  case 4:
+    day = "Kemis";
+    break;
+  case 5:
+    day = "Jum'at";
+    break;
+  case 6:
+    day = "Sabtu";
+    break;
+}
+
+console.log(day);
+
+// Noted Di atas merupakan contoh logika sederhana daru
+// penerapan Switch Case
