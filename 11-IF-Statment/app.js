abfdc5b28fe49914f53747bb737f755f1ea20434
Ramdoni007 <ramd42156@gmail.com>
2020-10-30 08:02:13 +0700

    Penambahan Materi Ke 12 seputar If statment

--- 11-IF-Statment/app.js
+++ 11-IF-Statment/app.js
@@ -3,9 +3,38 @@
 const id = 100;
 
 if (id == 100) {
-  val = "Aku Baik Baik saja ";
+  console.log("Bener Yeay!!!");
 } else {
-  val = "aku tidak baik baik saja ";
+  console.log("Anda Kurang Beruntung Silahkan coba lagi nanti!!!!");
 }
 
-console.log(id);
+// Lebih Besar, Lebih Kecil
+
+if (id >= 100) {
+  console.log("Betul");
+} else {
+  console.log("Salah");
+}
+
+// If dengan String Type data if , else if and else
+
+const color = "blue";
+
+if (color == "blue") {
+  console.log("Yeay Betul ini adalah warna blue");
+} else if (color == "black") {
+  console.log("Ini warna Hitam");
+} else {
+  console.log("Opps ini bukan kedua nya mungkin ini warna lain !!!");
+}
+
+const name = "Rem";
+const age = 12;
+
+if (age > 0 && age < 12) {
+  console.log(`${name} bukan Istri Downy`);
+} else if (age > 0 && age == 12) {
+  console.log(`${name} Adalah Istri DOwny di Dunia Pararel Nanti`);
+} else {
+  console.log("Emilia adalah Istri Downy");
+}
