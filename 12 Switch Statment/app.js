28ded5bd3be3d3fb28b3be7772469d201b63acb3
Ramdoni007 <ramd42156@gmail.com>
2020-10-30 09:15:40 +0700

    Penambahan Folder Ke-12

---
+++ 12 Switch Statment/app.js
@@ -0,0 +1,9 @@
+console.log("Hellow DOWNY");
+console.log(12345);
+console.log(true);
+var greeting = "Salam Kenal!";
+console.log(greeting);
+console.log([1, 2, 3, 4]); // Ini Arrays
+console.log({ a: 1, b: 2, c: 3 });
+console.table({ a: 1, b: 2, c: 3 });
+console.error("This Is Error");
